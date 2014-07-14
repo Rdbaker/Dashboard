8f3db430f675a0c8eb845401ca0bec0858a91e11
Ryan Baker <ryan.da.baker@gmail.com>
2014-07-13 23:26:41 -0400

    fix heroku migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713181951) do
+ActiveRecord::Schema.define(version: 20140711221254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,7 @@ ActiveRecord::Schema.define(version: 20140713181951) do
     t.string   "source"
     t.string   "feed"
     t.string   "link"
-    t.string   "date_posted"
-    t.text     "content"
+    t.string   "content"
   end
 
 end
