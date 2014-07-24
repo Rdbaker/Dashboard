ae1a871d69ebb4dbda269141040ce51fa7e35725
Ryan Baker <ryan.da.baker@gmail.com>
2014-07-23 23:45:16 -0400

    adds npr logo and some reddit functionality

--- app/assets/javascripts/backbone/views/articles.js.coffee
+++ app/assets/javascripts/backbone/views/articles.js.coffee
@@ -17,12 +17,13 @@ $ ->
       table = $('#feed')
       for article in data
         article.date_posted = @formatDate(article)
+        article.src_pic = @getImageFromSource(article.source)
         table.append(JST['backbone/templates/articles_view']({article: article}))
         $('#stories').append(JST['backbone/templates/articles_modal']({article: article}))
 
     getImageFromSource: (source) =>
       src_map =
-        'NPR': 'assets/npr_logo.jpg'
+        'NPR': '/npr_logo.jpg'
       src_map[source]
 
     formatDate: (article) =>
--- app/services/reddit_request_service.rb
+++ app/services/reddit_request_service.rb
@@ -1,7 +1,62 @@
 class RedditRequestService < APIRequestService
   def initialize
+    @url = "http://www.reddit.com/r/"
+    @params = {
+      :format => "json"
+    }
+    @subreddits = []
+  end
+
+  def tech_subs
+    programming_sub
+    js_sub
+    rust_sub
+    algies_sub
+    python_sub
+    vim_sub
+    data_sub
+    sql_sub
   end
 
   def make_request
+    delete_existing
+    total_res = []
+    @subreddits.each do |sub|
+      send_to = @url + sub
+      res = api_request(send_to, @params)
+  end
+
+  private
+
+  def programming_sub
+    @subreddits.push('programming/hot.json?sort=hot')
+  end
+
+  def js_sub
+    @subreddits.push('javascript/hot.json?sort=hot')
+  end
+
+  def rust_sub
+    @subreddits.push('rust/hot.json?sort=hot')
+  end
+
+  def algies_sub
+    @subreddits.push('algorithms/hot.json?sort=hot')
+  end
+
+  def python_sub
+    @subreddits.push('Python/hot.json?sort=hot')
+  end
+
+  def vim_sub
+    @subreddits.push('vim/hot.json?sort=hot')
+  end
+
+  def data_sub
+    @subreddits.push('dataisbeautiful/hot.json?sort=hot')
+  end
+
+  def sql_sub
+    @subreddits.push('sql/hot.json?sort=hot')
   end
 end
